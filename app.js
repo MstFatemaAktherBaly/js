3f2a5fcaa30f5cee5c0045bd93ed634f76b17c9e
MstFatemaAktherBaly <117789188+MstFatemaAktherBaly@users.noreply.github.com>
2023-02-01 09:56:19 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// alert();
+// let x=23;
+// document.write(x%=9)
+let num=80
+if(num >=80 && num <=100){
+    document.write('')
+} else if(num >=70 && num <80){ document.write('A')}
+else if(num >=60 && num <70){ document.write('A-')}
+else if(num >=50 && num <60){ document.write('B')}
+else if(num >=40 && num <50){ document.write('C')}
+else if(num >=33 && num <40){ document.write('D')}
+else if(num >=0 && num <33){ document.write('F')}
+
+function GPA() {
+    document.write('A+')
+}
+
+document.getElementById(pic).function.src= 'pic_bulbon.gif on.gif'
+document.getElementById(pic)
